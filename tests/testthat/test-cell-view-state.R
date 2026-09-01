aa47e4503315b08a012a7f4716e72dfd67e204d4
xuesong wang <wangxuesong29@gmail.com>
2026-09-01 15:04:07 +0200

    test: resolve installed viewer assets
    Reuse the shared Viewer path resolver so JavaScript state tests work from both source and installed package layouts.

--- tests/testthat/test-cell-view-state.R
+++ tests/testthat/test-cell-view-state.R
@@ -1,12 +1,4 @@
-inst_candidates <- c(
-  normalizePath("inst", mustWork = FALSE),
-  normalizePath("../../inst", mustWork = FALSE),
-  normalizePath(testthat::test_path("../../inst"), mustWork = FALSE)
-)
-inst_dir <- inst_candidates[file.exists(file.path(inst_candidates, "viewer"))][
-  1
-]
-state_file <- file.path(inst_dir, "viewer", "www", "cell_views_state.js")
+state_file <- viewer_test_path("www", "cell_views_state.js")
 
 run_state_node <- function(body) {
   skip_if(Sys.which("node") == "", "node not on PATH")
